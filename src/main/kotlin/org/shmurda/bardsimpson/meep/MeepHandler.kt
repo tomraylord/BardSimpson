4eb32e4414b67eb52174a6c038d9e95cf44f99d0
tom <tom@shmurda.org>
2022-07-15 16:06:51 +0100

    Add future functions to meep handler

--- src/main/kotlin/org/shmurda/bardsimpson/meep/MeepHandler.kt
+++ src/main/kotlin/org/shmurda/bardsimpson/meep/MeepHandler.kt
@@ -1,17 +1,23 @@
 package org.shmurda.bardsimpson.meep
 
+import com.mongodb.client.MongoCollection
 import net.dv8tion.jda.api.JDA
+import org.bson.Document
 import org.shmurda.bardsimpson.Bard
 
 class MeepHandler(private val jda: JDA) {
 
     init {
+        loadAll()
+    }
+
+    private fun loadAll() {
 
     }
 
     companion object {
         var meeps: HashMap<String, Meep> = HashMap()
-        var collection = Bard.bard.mongo.getCollection("meeps")
+        var collection: MongoCollection<Document> = Bard.bard.mongo.getCollection("meeps")
     }
 
 }
